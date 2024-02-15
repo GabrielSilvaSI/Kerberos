c7ccf87c69f5ade02a76268fb61ed51ad9839ba9
Gabriel da Silva Lima <90117166+GabrielSilvaSI@users.noreply.github.com>
2024-02-14 23:56:32 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kerberos/SignupActivity.kt
@@ -0,0 +1,29 @@
+package com.example.kerberos
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class SignupActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        auth = Firebase.auth
+        checkAuth()
+    }
+
+    private fun checkAuth(){
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intent = Intent(this, ListActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
